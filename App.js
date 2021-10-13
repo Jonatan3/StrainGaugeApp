b6df24d4da8670ab8959cab28c96a814e81b8367
hbkworld\ext_JDAHL <jonatandahl99@gmail.com>
2021-10-13 12:29:20 +0200

    Small changes to check github connection is valid.

--- App.js
+++ App.js
@@ -1,112 +1,59 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity} from 'react-native'
+//import AddPeople from './components/addPeople';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+export default function App() {
+  const [people, setPeople] = useState([
+    { name: 'Jonatan', id: '1' },
+    { name: 'Joni', id: '2' },
+    { name: 'Jonabob', id: '3' },
+    { name: 'Jonder', id: '4' },
+    { name: 'Jobob', id: '5' },
+    { name: 'Jondai', id: '6' },
+    { name: 'Jona', id: '7' },
+  ])
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const pressedListElement = (id) => {
+    console.log(id + " clicked!")
+    setPeople((prevPeople) => {
+      return prevPeople.filter(people => people.id != id);
+    })
+  }
+  
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      
+      <View style={styles.container}>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          data={people}
+          renderItem={({ item }) => (
+            <TouchableOpacity onPress={() => pressedListElement(item.id)}>
+              <Text style={styles.item}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
     </View>
   );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    //flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    paddingBottom: 24,
   },
-  sectionTitle: {
+  item: {
+    marginTop: 24,
+    marginRight: 24,
+    marginLeft: 24,
+    padding: 30,
     fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 8,
+  }
+})
