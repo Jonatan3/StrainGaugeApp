6395df087c7a5f0a772e8ebfa1dbed1648fb121c
hbkworld\ext_JDAHL <jonatandahl99@gmail.com>
2021-11-01 14:09:34 +0100

    First iteration of the qrscanner done.

--- components/QRCodeScannerScreen.js
+++ components/QRCodeScannerScreen.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import QRCodeScanner from 'react-native-qrcode-scanner'
 import { View, ToastAndroid, Dimensions } from 'react-native'
+import StreamingConstructor from '../constructors/StreamingConstructor';
 
 export default function QRCodeScannerScreen({ route, navigation }) {
 
   const onSucces = (e) => {
-    ToastAndroid.show('Scanned Strain Gauge: ' + e.data, ToastAndroid.LONG)
-    navigation.push('Strain Gauge', {itemId: parseInt(e.data)})
+    if (StreamingConstructor.getDummyDataById(parseInt(e.data)) !== null) {
+      navigation.push('Strain Gauge', {itemId: parseInt(e.data)})
+    }
+    else {
+      ToastAndroid.show('ERROR! no strain gauge with id of ' + e.data + ' found!', ToastAndroid.LONG)
+      setTimeout(() => { this.scanner.reactivate() }, 3000)
+    }
   }
 
   return (
@@ -14,6 +20,7 @@ export default function QRCodeScannerScreen({ route, navigation }) {
       <QRCodeScanner 
         onRead={onSucces}
         cameraStyle={{ height: Dimensions.get('window').height }} 
+        ref={(node) => { this.scanner = node }}
       />
     </View>
   )
