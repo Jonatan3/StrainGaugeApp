3769a05f6dd59671d1d4bc0c61997c0e6a9b4da3
hbkworld\ext_JDAHL <jonatandahl99@gmail.com>
2021-11-04 12:41:21 +0100

    Moved SVg files, and fixed issues with them.

--- components/QRCodeScannerScreen.js
+++ components/QRCodeScannerScreen.js
@@ -1,40 +1,61 @@
 import React from 'react'
 import QRCodeScanner from 'react-native-qrcode-scanner'
+import { RNCamera } from 'react-native-camera'
 import { View, ToastAndroid, Dimensions, Button, Text } from 'react-native'
 import StreamingConstructor from '../constructors/StreamingConstructor'
 import { TouchableWithoutFeedback } from 'react-native'
-import { Arrow } from '../resources/SVGs'
+import { Arrow, Flash_off, Fslash_off } from '../resources/SVGs'
 
 
 export default function QRCodeScannerScreen({ route, navigation }) {
-
-  const onSucces = (e) => {
-    if (StreamingConstructor.getDummyDataById(parseInt(e.data)) !== null) {
-      navigation.push('Strain Gauge', { itemId: parseInt(e.data) })
-    }
-    else {
-      ToastAndroid.show('ERROR! no strain gauge with id of ' + e.data + ' found!', ToastAndroid.LONG)
-      setTimeout(() => { this.scanner.reactivate() }, 3000)
-    }
-  }
+  
 
   return (
     <View>
       <QRCodeScanner
+        flashMode={RNCamera.Constants.FlashMode.torch}
         showMarker={true}
         onRead={onSucces}
         cameraStyle={{ height: Dimensions.get('window').height }}
         ref={(node) => { this.scanner = node }}
         customMarker={
-          <View>
-            <TouchableWithoutFeedback onPress={() => console.log('Goddag')} style={{ height: 60, width: 60, }} > 
-              <Arrow style={{ height: 60, width: 60, }} />
-            </TouchableWithoutFeedback>
-
-            <Button title='HEJSASASbA' onPress={() => console.log('Hej')}/>
+          <View style={{ flex: 1, flexDirection: 'row' }}>
+            <View style={{ marginTop: 24, flex: 1, justifyContent: 'space-around', flexDirection: 'row' }} >
+              <TouchableWithoutFeedback onPress={() => navigation.pop()} >
+                <View>
+                  <Arrow style={{ justifyContent: 'space-around' }} />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+            <View style={{ flex: 4, justifyContent: 'space-around', flexDirection: 'row' }}>
+              <Text style={{ marginTop: 24, color: '#fff', fontSize: 16 }}>
+                Scan QR to navigate to channel
+              </Text>
+            </View>
+            <View style={{ marginTop: 24, flex: 1, justifyContent: 'space-around', flexDirection: 'row' }}>
+              <TouchableWithoutFeedback onPress={() => RNCamera.Constants.FlashMode.torch} >
+                <View>
+                  <Flash_off />
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
           </View>
         }
       />
     </View>
   )
+}
+
+const onFlashClick = (e) => {
+
+}
+
+const onSucces = (e) => {
+  if (StreamingConstructor.getDummyDataById(parseInt(e.data)) !== null) {
+    navigation.push('Strain Gauge', { itemId: parseInt(e.data) })
+  }
+  else {
+    ToastAndroid.show('ERROR! no strain gauge with id of ' + e.data + ' found!', ToastAndroid.LONG)
+    setTimeout(() => { this.scanner.reactivate() }, 3000)
+  }
 }
